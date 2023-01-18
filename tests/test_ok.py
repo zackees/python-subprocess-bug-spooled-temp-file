0e70b66c7ae0868d45132105d233871a365495a5
zackees <zachvorhies@protonmail.com>
2023-01-18 11:24:51 -0500

    update in.txt

--- tests/test_ok.py
+++ tests/test_ok.py
@@ -8,7 +8,7 @@ HERE = os.path.dirname(__file__)
 class Tester(unittest.TestCase):
     def test_ok(self):
         """This test passes on windows, linux and macos"""
-        with open(os.path.join(HERE, "in4.txt"), encoding="utf-8") as stream:
+        with open(os.path.join(HERE, "in.txt"), encoding="utf-8") as stream:
             p = subprocess.Popen(
                 "python accept_input.py",
                 cwd=HERE,
