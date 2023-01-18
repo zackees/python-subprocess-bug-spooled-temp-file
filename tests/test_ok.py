d9fdfe221f06fb413f9cb51e7b4a3b8cd54f3384
zackees <zachvorhies@protonmail.com>
2023-01-18 12:42:09 -0500

    adds another test case

--- tests/test_ok.py
+++ tests/test_ok.py
@@ -2,6 +2,8 @@ import unittest
 import os
 import subprocess
 
+from tempfile import TemporaryFile
+
 
 HERE = os.path.dirname(__file__)
 
@@ -20,6 +22,24 @@ class Tester(unittest.TestCase):
             p.wait()
             stdout = p.stdout.read()
             self.assertIn("OK", stdout)
+    
+    def test_ok_temp_file(self):
+        """This test passes on windows, linux and macos"""
+        with TemporaryFile() as stream:
+            stream.write("y\n".encode("utf-8"))
+            stream.seek(0)
+            p = subprocess.Popen(
+                "python accept_input.py",
+                cwd=HERE,
+                shell=True,
+                stdin=stream,
+                stdout=subprocess.PIPE,
+                universal_newlines=True
+            )
+            p.wait()
+            stdout = p.stdout.read()
+            self.assertIn("OK", stdout)
+
 
 if __name__ == "__main__":
     unittest.main()
